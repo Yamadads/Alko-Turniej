a39d070a15d56094e7a4054813c4f5bbacd47614
Yamadads <spawlak@yahoo.com>
2016-05-18 18:21:06 +0200

    new init

--- AI/AI/urls.py
+++ AI/AI/urls.py
@@ -1,4 +1,4 @@
-"""AlkoTurniej URL Configuration
+"""AI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from AlkoTurniej.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', main_page)
 ]
---
+++ AI/AlkoTurniej/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlkoturniejConfig(AppConfig):
+    name = 'AlkoTurniej'
